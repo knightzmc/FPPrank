fd28188166e7b2a6bcd33ad82201f556973044c3
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-02-10 18:28:04 +0000

    port 80

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open System.Net
 open Suave
 open Suave.Filters
 open Suave.Successful
@@ -26,19 +27,11 @@ let handle =
         let randUrl = randFrom urls random |> (+) "https://"
         moved_permanently randUrl)
 
-let inverseLerp a b value =
-    if a = b then 0.0 else max 0.0 value |> min 1.0
-
-let pattern pred x = if pred x then Some () else None
-let (|Negative|_|) = pattern ((<) 0)
-let (|MoreThan1|_|) = pattern ((>) 1)
-let clamp value =
-    match value with
-    | Negative -> 0.0
-    | MoreThan1 -> 1.0
-    | _ -> float value
-
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig handle
+    let config =
+        { defaultConfig with
+              bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 80 ] }
+
+    startWebServer config handle
     0 // return an integer exit code
