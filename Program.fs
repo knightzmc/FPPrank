6a8e3d6b1e13cabc3812f97964e2b0d864695b01
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-02-10 20:03:14 +0000

    Temporary redirect

--- Program.fs
+++ Program.fs
@@ -1,11 +1,12 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
-open System.Net
+open System
+open System.IO
 open Suave
-open Suave.Filters
-open Suave.Successful
-open Suave.Intermediate
+open Suave.Headers
+open Suave.Response
+open Suave.Http
 open Suave.Redirection
 
 let urls =
@@ -22,11 +23,13 @@ let nth arr i = Array.item i arr
 let randFrom (arr: 'a []) (random: Random) =
     Array.length arr |> random.Next |> nth arr
 
-let handle =
-    context (fun _ ->
-        let randUrl = randFrom urls random |> (+) "https://"
-        moved_permanently randUrl)
+let urlResponse _ =
+    let randUrl = randFrom urls random |> (+) "https://"
+    redirect randUrl
+    
+let handle = context (urlResponse)
 
+    
 [<EntryPoint>]
 let main _ =
     let config =
